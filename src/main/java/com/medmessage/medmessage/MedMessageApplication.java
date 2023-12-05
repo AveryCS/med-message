21afac81087343cadd1c1efa4c0d31dd8bcee018
AveryCS <averydcs@gmail.com>
2023-12-05 13:25:34 -0500

    Init commit

---
+++ src/main/java/com/medmessage/medmessage/MedMessageApplication.java
@@ -0,0 +1,13 @@
+package com.medmessage.medmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedMessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medmessage/medmessage/MedMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medmessage.medmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
