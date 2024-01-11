ae771f0aeff1f89a2f08e9965959d93e485f445a
AveryCS <averydcs@gmail.com>
2024-01-11 18:59:17 -0500

    Adding Voice Message controller

--- src/main/java/com/medmessage/medmessage/MedMessageApplication.java
+++ src/main/java/com/medmessage/medmessage/MedMessageApplication.java
@@ -9,5 +9,7 @@ public class MedMessageApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MedMessageApplication.class, args);
 	}
-
+//Sending SMS messages: POST /2010-04-01/Accounts/{TestAccountSid}/Messages
+	//task scheduler class
+	//pass in phone number and message
 }
---
+++ src/main/java/com/medmessage/medmessage/controllers/VoiceMessageController.java
@@ -0,0 +1,12 @@
+package com.medmessage.medmessage.controllers;
+
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VoiceMessageController {
+
+
+
+}
