69e51b8526e02e8909d4fe6ef87bcce33386211b
AveryCS <averydcs@gmail.com>
2024-01-11 20:08:42 -0500

    Call being made to phone when hitting the testtwilio endpoint

--- src/main/java/com/medmessage/medmessage/MedMessageApplication.java
+++ src/main/java/com/medmessage/medmessage/MedMessageApplication.java
@@ -1,5 +1,10 @@
 package com.medmessage.medmessage;
 
+import com.medmessage.medmessage.controllers.VoiceMessageController;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Call;
+import com.twilio.type.PhoneNumber;
+import java.net.URI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,9 +12,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MedMessageApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MedMessageApplication.class, args);
+
+
 	}
 //Sending SMS messages: POST /2010-04-01/Accounts/{TestAccountSid}/Messages
 	//task scheduler class
 	//pass in phone number and message
+
+
 }
--- src/main/java/com/medmessage/medmessage/controllers/VoiceMessageController.java
+++ src/main/java/com/medmessage/medmessage/controllers/VoiceMessageController.java
@@ -1,12 +1,46 @@
 package com.medmessage.medmessage.controllers;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Call;
+import com.twilio.type.PhoneNumber;
+import java.net.URI;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-public class VoiceMessageController {
+
+public class VoiceMessageController  {
+    @Autowired
+    public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
 
 
+    public  void createCall(){
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-}
+        Call call = Call.creator(
+                //to
+                        new com.twilio.type.PhoneNumber("+12514228131"),
+                        //from
+                        new com.twilio.type.PhoneNumber("+18336584003"),
+                        URI.create("http://demo.twilio.com/docs/voice.xml"))
+                .create();
+
+        System.out.println(call.getSid());
+    }
+
+    @GetMapping("/testtwilio")
+    public void makeCall(){
+        System.out.println("--------------------------------"+ ACCOUNT_SID);
+        System.out.println("--------------------------------"+ AUTH_TOKEN);
+        createCall();
+
+
+    }
+
+    }
+
